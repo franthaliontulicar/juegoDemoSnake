2e92f1e47ffb87b1c23862f0fc5b1afce9a0907c
paco <franmenendezpouso@gmail.com>
2016-04-05 10:50:41 +0200

    clase Segment completa

--- Segment.java
+++ Segment.java
@@ -1,4 +1,4 @@
-
+import java.awt.Color;
 /**
  * Write a description of class Segment here.
  * 
@@ -8,15 +8,22 @@
 public class Segment
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private int xPosicion;
+    private int yPosicion;
+    public final static int LONGITUD_SEGMENTO = 4;
+    private int direccion;
+    private Color color;
 
     /**
      * Constructor for objects of class Segment
      */
-    public Segment()
+    public Segment(int xPos, int yPos, Color col, int direc)
     {
         // initialise instance variables
-        x = 0;
+        xPosicion = xPos;
+        yPosicion = yPos;
+        color = col;
+        direccion = direc;
     }
 
     /**
@@ -25,9 +32,62 @@ public class Segment
      * @param  y   a sample parameter for a method
      * @return     the sum of x and y 
      */
-    public int sampleMethod(int y)
+    public int getXPosicion()
     {
-        // put your code here
-        return x + y;
+        return xPosicion;
+    }
+
+    public int xPosicionFinal(){
+        int xPosFinal = xPosicion;
+        if(getDireccion()== 0){
+            xPosFinal = xPosicion + LONGITUD_SEGMENTO;
+        }
+        else if (getDireccion() == 180){
+            xPosFinal = xPosicion - LONGITUD_SEGMENTO;
+        }
+        return xPosFinal;
+
+    }
+
+    public int getYPosicion()
+    {
+        return yPosicion;
+    }
+
+    public int yPosicionFinal(){
+
+        int yPosFinal = yPosicion;
+        if(getDireccion()== 90){
+            yPosFinal = yPosicion + LONGITUD_SEGMENTO;
+        }
+        else if (getDireccion() == 270){
+            yPosFinal = yPosicion - LONGITUD_SEGMENTO;
+        }
+        return yPosFinal;
+
+    }
+
+    public int getDireccion(){
+        return direccion;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void dibujar(Canvas lienzo){
+        lienzo.setForegroundColor(color);
+        lienzo.drawLine(xPosicion, yPosicion, xPosicionFinal(), yPosicionFinal());
+
+    }
+
+    public void borrar(Canvas lienzo){
+        lienzo.erase();
+    }
+    
+    public boolean colisiona(Segment segmento){
+        
+        return (xPosicionFinal() == segmento.getXPosicion() && yPosicionFinal() == segmento.getYPosicion() );
+            
     }
 }
